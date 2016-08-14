6b3a1c53232df0e74a60c792c89bdff890c35b8c
Michał Budzyński <michal@virtualdesign.pl>
2016-08-14 11:48:33 +0200

    :briefcase: module separation

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 'use strict';
 
-var path = require('path');
-var fs=require('fs');
+var getFiles = require('./utils/getFiles');
 
-var getFiles = function(startPath, filter, callback) {
-
-  if (!fs.existsSync(startPath)){
-      console.log('Directory not found - ',startPath);
-      return;
-  }
-
-  var files = fs.readdirSync(startPath);
-  for(var i=0; i<files.length ;i++) {
-    var filename = path.join(startPath,files[i]);
-    var stat = fs.lstatSync(filename);
-    if (stat.isDirectory()) {
-        getFiles(filename,filter,callback); //recurse
-    } else if (filter.test(filename)){
-       callback(filename);
-    }
-  }
-};
-
-getFiles('./trash',/\.png$/,function(filename) {
-  console.log('-- found: ',filename);
+getFiles('./trash', /\.png$/, function(file) {
+  console.log('hello', file);
 });
-
-module.exports = getFiles;
